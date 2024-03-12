b9220f89e23bc7fc2a6e5bf98cf6509eb757af9f
jokyuhwan <rbghksdlsms@gmail.com>
2024-03-12 09:06:01 +0900

    이미지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
+    import { fade, blur, fly, slide, scale, draw } from 'svelte/transition';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 in:fade="{{delay: 1000}}" out:fade="{{duration: 1000}}">Welcome to SvelteKit</h1>
+<p in:fade="{{delay: 1000}}" out:fade="{{duration: 1000}}">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -8,21 +8,21 @@
 
     .gallery {
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
         gap: 16px;
         padding: 16px;
     }
 
     .gallery img {
         width: 100%;
-        height: auto;
+        height: 300px;
         border-radius: 8px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
         transition: transform 0.3s ease-in-out;
     }
 
     .gallery img:hover {
-        transform: scale(1.1);
+        transform: scale(2);
     }
 </style>
 <script>
@@ -33,9 +33,21 @@
 <!-- <h1 in:fade="{{delay: 1000}}" out:fade="{{duration: 1000}}">project</h1> -->
 
 <div class="gallery" in:fade="{{delay: 1000}}" out:fade="{{duration: 1000}}">
-    <img class="topBg" src="" alt="사진 1">
+    <img class="topBg" src="/src/lib/images/inte1.jpeg" alt="사진 1">
     <img class="topBg" src="" alt="사진 2">
     <img class="topBg" src="" alt="사진 3">
-    <img class="topBg" src="" alt="사진 3">
+    <img class="topBg" src="" alt="사진 4">
+    <img class="topBg" src="" alt="사진 5">
+    <img class="topBg" src="" alt="사진 6">
+    <img class="topBg" src="" alt="사진 7">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
+    <img class="topBg" src="" alt="사진 8">
     <!-- 필요한 만큼 사진 추가 -->
 </div>
